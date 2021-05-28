b820041876b78b25291b0fd8c6599fa38c3002ff
Prince <62904535+princek0@users.noreply.github.com>
2021-05-28 19:40:23 +0100

    Update RandomFunctions.py

--- RandomFunctions.py
+++ RandomFunctions.py
@@ -51,3 +51,21 @@ fibonacci2 = lambda n: n if n <= 1 else fibonacci2(n - 1) + fibonacci2(n - 2)  #
 
 def triangular(n):  # Returns the nth triangular number.
     return (n ** 2 + n) / 2
+
+
+def roman_num(n):  # Converts denary numbers to roman numerals.
+    if n > 3999:
+        return None
+    else:
+        symbol_list = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+                       (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+                       (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+        answer = ""
+        while n > 0:
+            for i in range(len(symbol_list)):
+                floor = n // symbol_list[i][0]
+                remainder = n % symbol_list[i][0]
+                answer += symbol_list[i][1] * floor
+                n = remainder
+
+        return answer
